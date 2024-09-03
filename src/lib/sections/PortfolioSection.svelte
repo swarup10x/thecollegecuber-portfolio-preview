3e560123a1bf04a7ff7b7374a31a1bd168233a9b
Swarup Ahmed <swarup692@gmail.com>
2024-09-03 22:31:17 +0600

    third-commit

--- src/lib/sections/PortfolioSection.svelte
+++ src/lib/sections/PortfolioSection.svelte
@@ -153,7 +153,7 @@
         flex-direction: row-reverse;
         gap: 40px;
         padding: 0 6px;
-        margin-bottom: 80px;
+        margin-bottom: 100px;
     }
 
     @media screen and (max-width: 720px) {
